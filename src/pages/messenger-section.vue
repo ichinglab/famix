1d5caaccfb5fddd3620da32e2771ba891dc07411
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-06-08 04:23:35 +0600

    Update messenger-section.vue

--- src/pages/messenger-section.vue
+++ src/pages/messenger-section.vue
@@ -1,7 +1,39 @@
 <template>
   <div>
     <q-btn icon="arrow_back_ios_new" to="/" flat color="green"></q-btn>
-    <div class="row flex-center">Messenger Working</div>
+    <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+          :text="['hey, how are you?']"
+          stamp="7 minutes ago"
+          sent
+          bg-color="amber-7"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+          :text="[
+            'doing fine, how r you?',
+            'I just feel like typing a really, really, REALLY long message to annoy you...',
+          ]"
+          size="6"
+          stamp="4 minutes ago"
+          text-color="white"
+          bg-color="primary"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+          :text="['Did it work?']"
+          stamp="1 minutes ago"
+          size="8"
+          text-color="white"
+          bg-color="primary"
+        />
+      </div>
+    </div>
   </div>
 </template>
 <script>
