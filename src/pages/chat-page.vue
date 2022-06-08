209b36b9cea06eb526f231a92d828331d22b9179
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-06-08 19:30:24 +0600

    update

--- src/pages/chat-page.vue
+++ src/pages/chat-page.vue
@@ -1,31 +1,119 @@
 <template>
   <div class="q-pa-md">
-    <q-list v-for="n in 15" :key="n"  separator class="text-weight-bolder">
-      <q-item to="messenger-section" clickable v-ripple>
+    <q-list v-for="n in 2" :key="n"  separator class="text-weight-bolder">
+      <q-item v-for="user in users" :key="user.id" to="messenger-section" clickable v-ripple>
         <q-item-section avatar>
           <q-avatar>
-            <q-img src="https://cdn.quasar.dev/img/avatar5.jpg"></q-img>
+            <img :src="`https://cdn.quasar.dev/img/${user.avator}`">
           </q-avatar>
         </q-item-section>
         <q-item-section>
-          <q-item-label>Name</q-item-label>
-          <q-item-label caption>Last Message</q-item-label>
+          <q-item-label>{{user.name}}</q-item-label>
+          <q-item-label caption>{{user.last_message}}</q-item-label>
         </q-item-section>
-          <q-item-label caption>
-            <q-btn round dence color="grey-9" size="6px" icon="done"></q-btn>
-          </q-item-label>
+        <q-item-label caption>
+          <q-btn round dence color="grey-9" size="6px" icon="done"></q-btn>
+        </q-item-label>
       </q-item>
     </q-list>
+    <!-- <NewChat /> -->
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import { ref } from "vue";
+// import NewChat from "src/components/new-chat.vue";
+const users = [
+  {
+    id: 1,
+    name: "Ruddy Jedrzej",
+    last_message: "Hey baby?",
+    avator: 'avatar1.jpg',
+  },
+  {
+    id: 2,
+    name: "Mallorie Alessandrini",
+    last_message: "where you? need you now.",
+    avator: 'avatar2.jpg',
+  },
+  {
+    id: 3,
+    name: "Elisabetta Wicklen",
+    last_message: "f*** bloody ",
+    avator: 'avatar3.jpg',
+  },
+  {
+    id: 4,
+    name: "Humayun Azad",
+    last_message: "Let's smoke for outside?",
+    avator: 'avatar4.jpg',
+  },
+  {
+    id: 5,
+    name: "Sara Chowdhury",
+    last_message: "Hey Rocky , what's up?",
+    avator: 'avatar5.jpg',
+  },
+  {
+    id: 5,
+    name: "Jeme Farhana",
+    last_message: "Hello baby?",
+    avator: 'avatar6.jpg',
+  },
+  {
+    id: 6,
+    name: "MD Nayeem",
+    last_message: "Dost koi tui?",
+    avator: 'avatar7.jpg',
+  },
+  {
+    id: 4,
+    name: "Seka Fawdrey",
+    last_message: "sfawdrey3@wired.com",
+    avator: 'avatar4.jpg',
+  },
+  {
+    id: 4,
+    name: "Seka Fawdrey",
+    last_message: "sfawdrey3@wired.com",
+    avator: 'avatar4.jpg',
+  },
+  {
+    id: 4,
+    name: "Seka Fawdrey",
+    last_message: "sfawdrey3@wired.com",
+    avator: 'avatar4.jpg',
+  },
+  {
+    id: 4,
+    name: "Seka Fawdrey",
+    last_message: "sfawdrey3@wired.com",
+    avator: 'avatar4.jpg',
+  },
+  {
+    id: 4,
+    name: "Seka Fawdrey",
+    last_message: "sfawdrey3@wired.com",
+    avator: 'avatar4.jpg',
+  },
+  {
+    id: 4,
+    name: "Seka Fawdrey",
+    last_message: "sfawdrey3@wired.com",
+    avator: 'avatar4.jpg',
+  },
+  {
+    id: 4,
+    name: "Seka Fawdrey",
+    last_message: "sfawdrey3@wired.com",
+    avator: 'avatar4.jpg',
+  },
+];
 export default defineComponent({
+  // components: { NewChat },
   setup() {
-    return {
-    };
+    return { users };
   },
 });
 </script>
