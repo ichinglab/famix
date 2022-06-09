57db3ea400b6e1002fc83a4420782206557733c8
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-06-10 03:03:14 +0600

    update

---
+++ src/components/create-post.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-input
+        v-model="textareaModel"
+        bottom-slots
+        outlined
+        clearable
+        autogrow
+        color="green"
+        label="Post Here"
+        :shadow-text="textareaShadowText"
+        @keydown="processTextareaFill"
+        @focus="processTextareaFill"
+      >
+        <template v-slot:before>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+          </q-avatar>
+        </template>
+        <template v-slot:after>
+          <q-btn round dense flat icon="send" />
+        </template>
+      </q-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import { event } from "quasar";
+import { ref, computed } from "vue";
+
+const { stopAndPrevent } = event;
+
+export default {
+  setup() {
+    const inputModel = ref("");
+    const inputFillCancelled = ref(false);
+    const inputShadowText = computed(() => {
+      if (inputFillCancelled.value === true) {
+        return "";
+      }
+
+      const t = "Text filled when you press TAB";
+      const empty =
+        typeof inputModel.value !== "string" || inputModel.value.length === 0;
+
+      if (empty === true) {
+        return t;
+      } else if (t.indexOf(inputModel.value) !== 0) {
+        return "";
+      }
+
+      return t.split(inputModel.value).slice(1).join(inputModel.value);
+    });
+
+    const textareaModel = ref("");
+    const textareaFillCancelled = ref(false);
+    const textareaShadowText = computed(() => {
+      if (textareaFillCancelled.value === true) {
+        return "";
+      }
+
+      const t =
+          "This text\nwill be filled\non multiple lines\nwhen you press TAB",
+        empty =
+          typeof textareaModel.value !== "string" ||
+          textareaModel.value.length === 0;
+
+      if (empty === true) {
+        return t.split("\n")[0];
+      } else if (t.indexOf(textareaModel.value) !== 0) {
+        return "";
+      }
+
+      return t
+        .split(textareaModel.value)
+        .slice(1)
+        .join(textareaModel.value)
+        .split("\n")[0];
+    });
+
+    return {
+      inputModel,
+      inputFillCancelled,
+      inputShadowText,
+
+      processInputFill(e) {
+        if (e === void 0) {
+          return;
+        }
+
+        if (e.keyCode === 27) {
+          if (inputFillCancelled.value !== true) {
+            inputFillCancelled.value = true;
+          }
+        } else if (e.keyCode === 9) {
+          if (
+            inputFillCancelled.value !== true &&
+            this.inputShadowText.length > 0
+          ) {
+            stopAndPrevent(e);
+            inputModel.value =
+              (typeof inputModel.value === "string" ? inputModel.value : "") +
+              this.inputShadowText;
+          }
+        } else if (inputFillCancelled.value === true) {
+          inputFillCancelled.value = false;
+        }
+      },
+
+      textareaModel,
+      textareaFillCancelled,
+      textareaShadowText,
+
+      processTextareaFill(e) {
+        if (e === void 0) {
+          return;
+        }
+
+        if (e.keyCode === 27) {
+          if (textareaFillCancelled.value !== true) {
+            textareaFillCancelled.value = true;
+          }
+        } else if (e.keyCode === 9) {
+          if (
+            textareaFillCancelled.value !== true &&
+            this.textareaShadowText.length > 0
+          ) {
+            stopAndPrevent(e);
+            textareaModel.value =
+              (typeof textareaModel.value === "string"
+                ? textareaModel.value
+                : "") + this.textareaShadowText;
+          }
+        } else if (textareaFillCancelled.value === true) {
+          textareaFillCancelled.value = false;
+        }
+      },
+    };
+  },
+};
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,60 +1,143 @@
 <template>
-  <div class="row q-pa-md flex-center">
-    <q-tabs
-      v-model="tab"
-      dense
-      narrow-indicator
-      active-color="green"
-      indicator-color="green"
-      class="row flex-center"
-    >
-      <q-tab name="feed" label="Feed" />
-      <q-tab name="jobs" label="Jobs" />
-      <q-tab name="friends" label="friends" />
-      <q-tab name="profile" label="profile" id="profile"/>
-    </q-tabs>
+  <div class="q-pa-md flex-center" style="max-width: 100%">
+    <div class="row flex-center" style="max-width: 100%">
+      <q-tabs
+        v-model="tab"
+        dense
+        narrow-indicator
+        active-color="green"
+        indicator-color="green"
+      >
+        <q-tab name="feed" label="Feed" />
+        <q-tab name="jobs" label="Jobs" />
+        <q-tab name="friends" label="friends" />
+        <q-tab name="profile" label="profile" />
+      </q-tabs>
+    </div>
+
+    <q-separator />
+
+    <div class="row flex-center">
       <q-tab-panels v-model="tab" animated>
+        <!-- Feed Section -->
+
         <q-tab-panel name="feed">
-          <div class="text-h6">Feeds</div>
-          <q-page-container>
-            <q-page class="q-pa-md">
-              <p v-for="n in 1500" :key="n">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-                nihil praesentium molestias a adipisci, dolore vitae odit,
-                quidem consequatur optio voluptates asperiores pariatur eos
-                numquam rerum delectus commodi perferendis voluptate?
-              </p>
-            </q-page>
-          </q-page-container>
+          <div row flex-center>
+            <!-- create post components -->
+            <div class="q-pa-md">
+              <createPost />
+            </div>
+            <!-- create post components -->
+            <div v-for="n in 15" :key="n" class="row flex-center">
+              <q-card class="bg-green my-card">
+                <q-item-section>
+                  <q-item-label>
+                    <div class="row">
+                      <div class="col">
+                        <q-avatar size="30px">
+                          <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                        </q-avatar>
+                        <span class="q-pa-md text-h6">Name</span>
+                        <span class="text-h7"> 2:04 AM</span>
+                      </div>
+                    </div>
+                  </q-item-label>
+                </q-item-section>
+                <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+                <q-card-section class="row">
+                  <div class="text-h6">Our Changing Planet</div>
+                  <q-space />
+                  <!-- <div class="text-subtitle2">by Name</div> -->
+                  <q-btn flat dense round color="primary" icon="favorite" />
+                  <q-btn flat dense round color="primary" icon="bookmark" />
+                  <q-btn flat dense round color="primary" icon="share" />
+                </q-card-section>
+                <q-card-section class="q-pt-none">
+                  {{ lorem }}
+                </q-card-section>
+                <q-card-actions>
+                  <q-space />
+                  <span class="read_more_text">READ MORE</span>
+                  <q-btn
+                    color="primary"
+                    round
+                    flat
+                    dense
+                    :icon="
+                      expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+                    "
+                    @click="expanded = !expanded"
+                  />
+                </q-card-actions>
+
+                <q-slide-transition>
+                  <div v-show="expanded">
+                    <q-separator />
+                    <q-card-section class="text-subitle2">
+                      {{ lorem }}
+                    </q-card-section>
+                  </div>
+                </q-slide-transition>
+              </q-card>
+            </div>
+          </div>
         </q-tab-panel>
+
+        <!-- Job Section -->
 
         <q-tab-panel name="jobs">
           <div class="text-h6">Jobs</div>
           Jobs Working !
         </q-tab-panel>
 
+        <!-- Friends Section -->
+
         <q-tab-panel name="friends">
           <div class="text-h6">Friends</div>
           Friends Working !
         </q-tab-panel>
+
+        <!-- Profile Section -->
+
         <q-tab-panel name="profile">
           <div class="text-h6">Profile</div>
           Profile Working !
         </q-tab-panel>
       </q-tab-panels>
     </div>
-    <q-separator />
+  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import { ref } from "vue";
+import createPost from "components/create-post.vue";
 export default defineComponent({
+  components: { createPost },
   name: "IndexPage",
   setup() {
     return {
       tab: ref("feed"),
+      lorem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      expanded: ref(false),
     };
   },
 });
 </script>
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 1250px
+
+.row > div
+  padding: 10px 15px
+  // background: rgba(86,61,124,.15)
+  // border: 1px solid rgba(86,61,124,.2)
+.row + .row
+  margin-top: 1rem
+
+.read_more_text
+ font-size: 10px
+</style>
