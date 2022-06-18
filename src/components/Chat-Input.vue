48eff1538558e0c1d1689af082479e66648353ca
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-06-18 08:14:10 +0600

    chat update

---
+++ src/components/Chat-Input.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-input
+        v-model="createPost"
+        bottom-slots
+        outlined
+        rounded
+        clearable
+        dense
+        autogrow
+        color="green"
+        label="Post Here"
+        :shadow-text="textareaShadowText"
+        @keydown="processTextareaFill"
+        @focus="processTextareaFill"
+      >
+        <template v-slot:before>
+          <q-avatar size="43px">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+          </q-avatar>
+        </template>
+        <template v-slot:after>
+          <q-btn round type="submit" size="25px" dense flat icon="send" />
+        </template>
+      </q-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import { event } from "quasar";
+import { ref, computed } from "vue";
+
+const { stopAndPrevent } = event;
+
+export default {
+  setup() {
+    const inputModel = ref("");
+    const inputFillCancelled = ref(false);
+    const inputShadowText = computed(() => {
+      if (inputFillCancelled.value === true) {
+        return "";
+      }
+
+      const t = "Text filled when you press TAB";
+      const empty =
+        typeof inputModel.value !== "string" || inputModel.value.length === 0;
+
+      if (empty === true) {
+        return t;
+      } else if (t.indexOf(inputModel.value) !== 0) {
+        return "";
+      }
+
+      return t.split(inputModel.value).slice(1).join(inputModel.value);
+    });
+
+    const createPost = ref("");
+    const textareaFillCancelled = ref(false);
+    const textareaShadowText = computed(() => {
+      if (textareaFillCancelled.value === true) {
+        return "";
+      }
+
+      const t =
+          "This text\nwill be filled\non multiple lines\nwhen you press TAB",
+        empty =
+          typeof createPost.value !== "string" ||
+          createPost.value.length === 0;
+
+      if (empty === true) {
+        return t.split("\n")[0];
+      } else if (t.indexOf(createPost.value) !== 0) {
+        return "";
+      }
+
+      return t
+        .split(createPost.value)
+        .slice(1)
+        .join(createPost.value)
+        .split("\n")[0];
+    });
+
+    return {
+      inputModel,
+      inputFillCancelled,
+      inputShadowText,
+
+      processInputFill(e) {
+        if (e === void 0) {
+          return;
+        }
+
+        if (e.keyCode === 27) {
+          if (inputFillCancelled.value !== true) {
+            inputFillCancelled.value = true;
+          }
+        } else if (e.keyCode === 9) {
+          if (
+            inputFillCancelled.value !== true &&
+            this.inputShadowText.length > 0
+          ) {
+            stopAndPrevent(e);
+            inputModel.value =
+              (typeof inputModel.value === "string" ? inputModel.value : "") +
+              this.inputShadowText;
+          }
+        } else if (inputFillCancelled.value === true) {
+          inputFillCancelled.value = false;
+        }
+      },
+
+      createPost,
+      textareaFillCancelled,
+      textareaShadowText,
+
+      processTextareaFill(e) {
+        if (e === void 0) {
+          return;
+        }
+
+        if (e.keyCode === 27) {
+          if (textareaFillCancelled.value !== true) {
+            textareaFillCancelled.value = true;
+          }
+        } else if (e.keyCode === 9) {
+          if (
+            textareaFillCancelled.value !== true &&
+            this.textareaShadowText.length > 0
+          ) {
+            stopAndPrevent(e);
+            createPost.value =
+              (typeof createPost.value === "string"
+                ? createPost.value
+                : "") + this.textareaShadowText;
+          }
+        } else if (textareaFillCancelled.value === true) {
+          textareaFillCancelled.value = false;
+        }
+      },
+    };
+  },
+};
+</script>
--- src/pages/messenger-section.vue
+++ src/pages/messenger-section.vue
@@ -1,43 +1,109 @@
 <template>
   <div>
-    <div class="q-pa-md row justify-center">
+    <div class="q-pa-md flex-center row">
       <div style="width: 100%; max-width: 1200px">
-        <q-chat-message
-          name="me"
-          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-          :text="['hey, how are you?']"
-          stamp="7 minutes ago"
-          sent
-          bg-color="amber-7"
-        />
-        <q-chat-message
-          name="Jane"
-          avatar="https://cdn.quasar.dev/img/avatar5.jpg"
-          :text="[
-            'doing fine, how r you?',
-            'I just feel like typing a really, really, REALLY long message to annoy you...',
-          ]"
-          size="6"
-          stamp="4 minutes ago"
-          text-color="white"
-          bg-color="primary"
-        />
-        <q-chat-message
-          name="Jane"
-          avatar="https://cdn.quasar.dev/img/avatar5.jpg"
-          :text="['Did it work?']"
-          stamp="1 minutes ago"
-          size="8"
-          text-color="white"
-          bg-color="primary"
-        />
-        <q-spinner-dots size="2rem" />
+        <div>
+          <q-chat-message
+            name=""
+            avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+            :text="['hey, how are you?']"
+            stamp="7 minutes ago"
+            sent
+            bg-color="amber-7"
+          />
+          <q-chat-message
+            name="Jane"
+            avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+            :text="[
+              'doing fine, how r you?',
+              'I just feel like typing a really, really, REALLY long message to annoy you...',
+            ]"
+            size="6"
+            stamp="4 minutes ago"
+            text-color="white"
+            bg-color="primary"
+          />
+          <q-chat-message
+            name="Jane"
+            avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+            :text="['Did it work?']"
+            stamp="1 minutes ago"
+            size="8"
+            text-color="white"
+            bg-color="primary"
+          />
+          <q-chat-message
+            v-for="(item, index) in submitResult"
+            :key="index"
+            name=""
+            avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+            :text="[item.value]"
+            stamp="just now"
+            sent
+            bg-color="amber-7"
+          />
+          <q-spinner-dots size="2rem" />
+        </div>
+
+        <!-- Chat Input -->
+
+        <div class="q-pa-md q-gutter-md fixed-bottom">
+          <q-form @submit="onSubmit">
+            <div class="row flex-center">
+              <div class="col-10">
+                <q-input
+                  v-model="name"
+                  bottom-slots
+                  name="name"
+                  outlined
+                  clearable
+                  rounded
+                  dense
+                  color="green"
+                >
+                  <template v-slot:before>
+                    <q-avatar size="43px">
+                      <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+                    </q-avatar>
+                  </template>
+                </q-input>
+              </div>
+              <div class="col-2">
+                <q-btn round type="submit" size="25px" dense flat icon="send" />
+              </div>
+            </div>
+          </q-form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { ref } from "vue";
+// import ChatInput from "components/Chat-Input.vue";
+
 export default {
-  // name: 'PageName',
+  setup() {
+    const submitResult = ref([]);
+
+    return {
+      name: ref(""),
+      submitResult,
+
+      onSubmit(evt) {
+        const formData = new FormData(evt.target);
+        const data = [];
+
+        for (const [name, value] of formData.entries()) {
+          data.push({
+            name,
+            value,
+          });
+        }
+
+        submitResult.value = data;
+      },
+    };
+  },
 };
 </script>
