354568b60305e818580e227ef5d1f27bc94e3f76
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-07-03 23:12:14 +0600

    Update Users-Profile.vue

--- src/components/Users-Profile.vue
+++ src/components/Users-Profile.vue
@@ -14,7 +14,7 @@
       </q-btn>
       <img
         src="../assets/avator/avatar2.jpeg"
-        style="max-width: 100%; max-height: 400px ; border-radius: 20px"
+        style="max-width: 100%; max-height: 400px; border-radius: 20px"
       />
       <div class="row flex-center my-pic" style="width: 100%">
         <q-avatar size="200px">
@@ -23,16 +23,113 @@
       </div>
     </div>
     <div class="row flex-center" style="padding: 100px">
-      <p class="tex-bold text-h5">Ruddy Tanna</p>
+      <p class="tex-bold text-h5">Pallob Vau</p>
+    </div>
+    <div class="flex-center">
+      <div class="q-pa-md">
+        <div style="max-width: 600px">
+          <q-tabs
+            v-model="tab"
+            align="justify"
+            narrow-indicator
+            class="q-mb-lg"
+          >
+            <q-tab class="text-purple" name="mails" label="Mails" />
+            <q-tab class="text-orange" name="alarms" label="Alarms" />
+            <q-tab class="text-teal" name="movies" label="Movies" />
+          </q-tabs>
+
+          <div class="q-gutter-y-sm">
+            <q-tab-panels
+              v-model="tab"
+              animated
+              transition-prev="scale"
+              transition-next="scale"
+              class="bg-purple text-white text-center"
+            >
+              <q-tab-panel name="mails">
+                <div class="text-h6">Mails</div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="alarms">
+                <div class="text-h6">Alarms</div>
+                Ad molestiae non facere animi nobis, similique nemo velit
+                reiciendis corporis impedit nam in.
+              </q-tab-panel>
+
+              <q-tab-panel name="movies">
+                <div class="text-h6">Movies</div>
+                Nostrum necessitatibus expedita dolores? Voluptatem repudiandae
+                magni ea.
+              </q-tab-panel>
+            </q-tab-panels>
+
+            <q-tab-panels
+              v-model="tab"
+              animated
+              transition-prev="fade"
+              transition-next="fade"
+              class="bg-orange text-white text-center"
+            >
+              <q-tab-panel name="mails">
+                <div class="text-h6">Mails</div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="alarms">
+                <div class="text-h6">Alarms</div>
+                Ad molestiae non facere animi nobis, similique nemo velit
+                reiciendis corporis impedit nam in.
+              </q-tab-panel>
+
+              <q-tab-panel name="movies">
+                <div class="text-h6">Movies</div>
+                Nostrum necessitatibus expedita dolores? Voluptatem repudiandae
+                magni ea.
+              </q-tab-panel>
+            </q-tab-panels>
+
+            <q-tab-panels
+              v-model="tab"
+              animated
+              transition-prev="jump-up"
+              transition-next="jump-down"
+              class="bg-teal text-white text-center"
+            >
+              <q-tab-panel name="mails">
+                <div class="text-h6">Mails</div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </q-tab-panel>
+
+              <q-tab-panel name="alarms">
+                <div class="text-h6">Alarms</div>
+                Ad molestiae non facere animi nobis, similique nemo velit
+                reiciendis corporis impedit nam in.
+              </q-tab-panel>
+
+              <q-tab-panel name="movies">
+                <div class="text-h6">Movies</div>
+                Nostrum necessitatibus expedita dolores? Voluptatem repudiandae
+                magni ea.
+              </q-tab-panel>
+            </q-tab-panels>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "usersProfile",
   setup() {
-    return {};
+    return {
+      // tab: ref("about"),
+      tab: ref("mails"),
+    };
   },
 };
 </script>
