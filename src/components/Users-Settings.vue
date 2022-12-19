b347a408c6889b607a010751b84dd51a5e361360
md_arobi_emon <94915353+arobiemon@users.noreply.github.com>
2022-12-19 22:28:42 +0600

    msg

--- src/components/Users-Settings.vue
+++ src/components/Users-Settings.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="q-pa-md" style="max-width: 1300px">
+    <div class="q-pa-md row">
+     <div class="text-h4">Account</div>
+     <div class="text-h7">Manage settings related to permissions, ads, Off-Facebook activity and other account information.</div>
+    <div class="q-p-md row ">
+      <div class="col-2">coto</div>
+      <div class="col-10">
+        <div class="collom">hhh</div>
+        <div class="collom">boro</div>
+      </div>
+    </div>
+    </div>
     <div class="q-py-md row flex-center">
       <q-btn
         class="glossy"
