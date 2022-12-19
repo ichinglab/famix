ef0f8e3e870b86055fe98bddba46a663f54d4213
md_arobi_emon <94915353+arobiemon@users.noreply.github.com>
2022-12-20 00:45:56 +0600

    update

--- src/components/Users-Settings.vue
+++ src/components/Users-Settings.vue
@@ -8,42 +8,198 @@
       </div>
       <div class="row q-py-sm">
         <div class="col-2">
-          <q-btn unelevated icon="account_balance"></q-btn>
+          <q-btn unelevated icon="account_circle"></q-btn>
         </div>
         <div class="col-10">
-          <div class="collom">hhh</div>
-          <div class="collom">Manage settings related to permissions,</div>
+          <div class="collom text-bold ">Parsonal information</div>
+          <div class="collom">Update your name and contact information connected to your Facebook account. </div>
         </div>
       </div>
       <div class="row q-py-sm">
         <div class="col-2">
-          <q-btn unelevated icon="account_balance"></q-btn>
+          <q-btn unelevated icon="translate"></q-btn>
         </div>
         <div class="col-10">
-          <div class="collom">hhh</div>
-          <div class="collom">Manage settings related to permissions,</div>
+          <div class="collom text-bold">Translation for posts</div>
+          <div class="collom">Let us know your translation preferences for posts from friends and bilingual posts.</div>
         </div>
       </div>
       <div class="row q-py-sm">
         <div class="col-2">
-          <q-btn unelevated icon="account_balance"></q-btn>
+          <q-btn unelevated icon="text_format"></q-btn>
         </div>
         <div class="col-10">
-          <div class="collom">hhh</div>
-          <div class="collom">Manage settings related to permissions,</div>
+          <div class="collom text-bold">Font size (Medium)</div>
+          <div class="collom">Choose the size of your font in the app.</div>
         </div>
       </div>
       <div class="row q-py-sm">
         <div class="col-2">
-          <q-btn unelevated icon="account_balance"></q-btn>
+          <q-btn unelevated icon="add_card"></q-btn>
         </div>
         <div class="col-10">
-          <div class="collom">hhh</div>
-          <div class="collom">Manage settings related to permissions,</div>
+          <div class="collom text-bold">Ads Payments</div>
+          <div class="collom">Make payments or edit your payment settings</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="add_card"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Facebook Pay</div>
+          <div class="collom">Manage your payment settings, see your payment history and access live chat support.</div>
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="text-h4 q-py-md">Security</div>
+      <div class="text-h7 q-py-md">
+        Manage settings related to permissions, ads, Off-Facebook activity and
+        other account information.
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="security"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold ">Security and login</div>
+          <div class="collom">Update your name and contact information connected to your Facebook account. </div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="app_registration"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Apps and website</div>
+          <div class="collom">Let us know your translation preferences for posts from friends and bilingual posts.</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="business_center"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Business intergration</div>
+          <div class="collom">Choose the size of your font in the app.</div>
+        </div>
+      </div>
 
+    </div>
+    <div class="row">
+      <div class="text-h4 q-py-md">Privacy</div>
+      <div class="text-h7 q-py-md">
+        Manage settings related to permissions, ads, Off-Facebook activity and
+        other account information.
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="lock"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold ">Privacy</div>
+          <div class="collom">Update your name and contact information connected to your Facebook account. </div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="lock_person"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Profile locking</div>
+          <div class="collom">Let us know your translation preferences for posts from friends and bilingual posts.</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="local_offer"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Profile and tagging</div>
+          <div class="collom">Choose the size of your font in the app.</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="post_add"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Public posts</div>
+          <div class="collom">Make payments or edit your payment settings</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="block"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Blocking</div>
+          <div class="collom">Manage your payment settings, see your payment history and access live chat support.</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="text-h4 q-py-md">Your Facebook information</div>
+      <div class="text-h7 q-py-md">
+        Manage settings related to permissions, ads, Off-Facebook activity and
+        other account information.
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="key"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold ">Access profile information</div>
+          <div class="collom">Update your name and contact information connected to your Facebook account. </div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="format_list_bulleted"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Activity log</div>
+          <div class="collom">Let us know your translation preferences for posts from friends and bilingual posts.</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="no_accounts"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Off-Facebook activity</div>
+          <div class="collom">Choose the size of your font in the app.</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="repeat_on"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Acces and control</div>
+          <div class="collom">Make payments or edit your payment settings</div>
+        </div>
+      </div>
+      <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="file_download"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Download profile information</div>
+          <div class="collom">Manage your payment settings, see your payment history and access live chat support.</div>
+        </div>
+      </div>
+    </div>
+    <div class="row q-py-sm">
+        <div class="col-2">
+          <q-btn unelevated icon="transfer_within_a_station"></q-btn>
+        </div>
+        <div class="col-10">
+          <div class="collom text-bold">Transfer a copy of your information</div>
+          <div class="collom">Manage your payment settings, see your payment history and access live chat support.</div>
+        </div>
+      </div>
     <div class="q-py-md row flex-center">
       <q-btn
         class="glossy"
